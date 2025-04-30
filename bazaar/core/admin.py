a9144028e88801ec8e82c76098de79004de53b44
bezbig <bezbig@yandex.ru>
2025-05-01 00:03:25 +0300

    Register custom user model

--- bazaar/core/admin.py
+++ bazaar/core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'city', 'region', 'address', 'phone', 'is_staff', 'is_active')}
+        ),
+    )
--- bazaar/core/models.py
+++ bazaar/core/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+
 # Create your models here.
 class CustomUser(AbstactUser):
     city = models.CharField(max_length=100, blank=True, null=True)
-    state = models.CharField(max_length=100, blank=True, null=True)
+    region = models.CharField(max_length=100, blank=True, null=True)
     address = models.TextField(blank=True, null=True)
     phone = models.CharField(max_length=15, blank=True, null=True)
 
